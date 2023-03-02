b3d649cd030d2e8b0f7e6f68d92b07e13c735cb7
iscgomal2csi2023@gmail.com <iscgomal2csi2023@gmail.com>
2023-03-02 12:14:27 +0100

    depot de mon projet

---
+++ TP_Laini_Mobile_Rachel/TP_Laini_Mobile_Rachel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_Laini_Mobile_Rachel
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
